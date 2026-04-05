b72fa48b6a46d229a5c4780059934fbe3c1c1de2
TaylorBundy <martonbarbosa@hotmail.com>
2026-04-05 04:43:16 -0300

    hola

--- app.py
+++ app.py
@@ -263,7 +263,7 @@ def mostrar_servicios():
         print("-" * 30)
 
     # 👇 NUEVO BLOQUE
-    print("==============================")
+    print("\n==============================")
     print(" PROCESOS DETECTADOS")
     print("==============================")
 
